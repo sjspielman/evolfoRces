84deb4698e9f631de4c1dd20c7cb5aca3a3dd431
Stephanie J. Spielman <spielman@rowan.edu>
2019-10-20 10:54:50 -0400

    updated run command to run_evolfo<rR>ces()

--- R/runapp.R
+++ R/runapp.R
@@ -1,7 +1,16 @@
 #' Run the evolfoRces Shiny application locally
 #'
 #' @export
-run <- function() {
+run_evolfoRces <- function() {
+  appDir <- system.file("evolfoRces", package = "evolfoRces")
+  shiny::runApp(appDir, display.mode = "normal")
+}
+
+
+#' Run the evolfoRces Shiny application locally, allowing for people to forget about cap "R"
+#'
+#' @export
+run_evolforces <- function() {
   appDir <- system.file("evolfoRces", package = "evolfoRces")
   shiny::runApp(appDir, display.mode = "normal")
 }
